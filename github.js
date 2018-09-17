dfcd7f70769d322c092a1faeda2aec44edb4461c
JackyLN <lenghia1991@gmail.com>
2018-09-17 15:26:23 +0800

    Update github.js

--- github.js
+++ github.js
@@ -1,1 +1,13 @@
 console.log("My github console");
+
+$( document ).ready(function() {    
+    document.getElementsByTagName("head")[0].insertAdjacentHTML(
+        "beforeend",
+        "<link rel=\"stylesheet\" href=\"https://sdks.shopifycdn.com/polaris/latest/polaris.css\" />");
+
+        var html = "<button type='button' class='Polaris-Button'><span class='Polaris-Button__Content'><span>Add Jackyy</span></span></button>";
+
+
+    $('#jackylestore').append(html);
+    //   <div style="height: 500px;"><button type="button" class="Polaris-Button"><span class="Polaris-Button__Content"><span>Open</span></span></button></div>
+});
